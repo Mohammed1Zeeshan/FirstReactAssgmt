948bb348bdfa3306fe895d349d30a48a8b3cab7e
mohammed zeeshan <mohdzeeshan276@gmail.com>
2022-02-24 14:02:54 +0530

    removed duplicate copies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 //<></>
 import logo from './logo.svg';
 import './App.css';
-
 import Home from './Components/Home';
 import Header from './Components/Header';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Userr from './Components/Userr';
 import AddUser from './Components/AddUser';
 import { Col, Container, Row } from 'react-bootstrap';
-import Vieww from './Components/Vieww';
-
 import User from './Components/User';
 import Edit from './Components/Edit';
 import Contact from './Components/Contact';
@@ -23,16 +20,15 @@ function App() {
     <Header/>
     <Row>
       <Col md='6'>
-      {/* <Route exact path='/all'> <Userr/></Route> */}
+      {/* <Route exact path='/all'> <Userr/></Route> <!-- avoided inorder to keep it fixed  on the page  -->*/}
       <Userr/>
         </Col>
-        <Col md='6'>
+        <Col md='6'>  
         <Route exact path='/'><Home/> </Route>
         <Route exact path='/Edit/:id'><Edit/> </Route>
         <Route exact path='/user/:id'><User/> </Route>
         <Route exact path='/users/add' > <AddUser></AddUser></Route>
         <Route exact path='/Contact' > <Contact></Contact></Route>
-        {/* <Route exact path='/users/:id'><Vieww/> </Route> */}
         </Col>  
         {/* <Col md='3'>
         
